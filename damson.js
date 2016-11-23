ad1674b841a0863d900a93f79a846cc66e36b24e
Tomas Liutvinas <TomasLiutvinas@gmail.com>
2016-11-23 18:44:10 +0200

    Init

---
+++ damson.js
@@ -0,0 +1,9 @@
+require ('./rescan.js');
+require ('./leltopkek.js');
+
+/*
+https://github.com/driverdan/node-XMLHttpRequest/blob/master/example/demo.js
+npm install xmlhttprequest
+npm install jquery
+npm install jsdom
+*/
